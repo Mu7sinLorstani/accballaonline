dae0b2dfd78df9cb4f180c2697b9be9144e2d397
Mu7sinLorstani <103287202+Mu7sinLorstani@users.noreply.github.com>
2022-11-17 15:40:01 +0300

    Update aff.py

--- aff.py
+++ aff.py
@@ -4,7 +4,7 @@ import discord, os, keep_alive, asyncio, datetime, pytz
 from discord.ext import tasks, commands
 
 client = commands.Bot(
-  command_prefix=':',
+  command_prefix='*',
   self_bot=True
 )
 
